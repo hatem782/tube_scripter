bc9405a2a3e301163752811ec369e647b5b0745b
GlowD03 <125704084+GlowD03@users.noreply.github.com>
2023-02-20 15:16:11 +0100

    Update chat_gpt.js

--- src/pages/ChatGpt/chat_gpt.js
+++ src/pages/ChatGpt/chat_gpt.js
@@ -1,14 +1,14 @@
 import { langs_en, langs_fr } from "../../assets/json/languages/languages";
 
 let items_en = [
-  { title: "14 years", value: "excited" },
+  { title: "-14 years", value: "excited" },
   { title: "14-24 years", value: "creative" },
   { title: "25-35 years", value: "encouraging" },
   { title: "+35 years", value: "professional" },
 ];
 
 let items_fr = [
-  { title: "14 ans", value: "excited" },
+  { title: "-14 ans", value: "excited" },
   { title: "14-24 ans", value: "creative" },
   { title: "25-35 ans", value: "encouraging" },
   { title: "+35 ans", value: "professional" },
@@ -36,13 +36,13 @@ const texts = {
     Title: "Title",
     Video_paragraph: "Script :",
     Hooks: "Hooks : ",
-    Video_description: "Video description : ",
-    Video_intro: "Video intro",
+    Video_description: "Description : ",
+    Video_intro: "Introduction : ",
 
     french: "French",
     english: "English",
-    mood: "Target audience age group :",
-    Target_audience_age_group: "Target audience age group",
+    mood: "Target audience :",
+    Target_audience_age_group: "Target audience",
     items: items_en,
     length: "Video length",
     langs: langs_en,
@@ -64,12 +64,12 @@ const texts = {
     Title: "Titre",
     Video_paragraph: "Script :",
     Hooks: "Hooks : ",
-    Video_description: "Description vidéo : ",
-    Video_intro: "Intro vidéo",
-    mood: "Audience ciblé tranche d'age :",
+    Video_description: "Description : ",
+    Video_intro: "Introduction : ",
+    mood: "Audience ciblé :",
     french: "Français",
     english: "Anglais",
-    Target_audience_age_group: "Groupe d'âge du public cible",
+    Target_audience_age_group: "Audience ciblé",
     items: items_fr,
     length: "Longeur de la video",
     langs: langs_fr,
