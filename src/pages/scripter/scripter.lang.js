c63bfa9c409334c3a11ad16ef00672b5d0a5dd0b
GlowD03 <125704084+GlowD03@users.noreply.github.com>
2023-02-20 15:14:18 +0100

    Update scripter.lang.js

--- src/pages/scripter/scripter.lang.js
+++ src/pages/scripter/scripter.lang.js
@@ -1,14 +1,14 @@
 import { langs_en, langs_fr } from "../../assets/json/languages/languages";
 
 let items_en = [
-  { title: "14 years", value: "excited" },
+  { title: "-14 years", value: "excited" },
   { title: "14-24 years", value: "creative" },
   { title: "25-35 years", value: "encouraging" },
   { title: "+35 years", value: "professional" },
 ];
 
 let items_fr = [
-  { title: "14 ans", value: "excited" },
+  { title: "-14 ans", value: "excited" },
   { title: "14-24 ans", value: "creative" },
   { title: "25-35 ans", value: "encouraging" },
   { title: "+35 ans", value: "professional" },
@@ -44,17 +44,17 @@ const texts = {
     Key_words: "Key words",
     Description: "Description :",
     Generate: "Generate",
-    EditChat: "Edit with TubeChat",
+    EditChat: "Personalize in your style",
     Title: "Title",
     Video_paragraph: "Script :",
     Hooks: "Hooks : ",
-    Video_description: "Video description : ",
-    Video_intro: "Video intro",
+    Video_description: "Description : ",
+    Video_intro: "Introduction : ",
 
     french: "French",
     english: "English",
-    mood: "Target audience age group :",
-    Target_audience_age_group: "Target audience age group",
+    mood: "Target audience :",
+    Target_audience_age_group: "Target audience",
     items: items_en,
     length: "Video length",
     langs: langs_en,
@@ -70,7 +70,7 @@ const texts = {
   fr: {
     YouTube_Scripter: "YouTube  Script",
     Tiktok_Scripter: "Tiktok  Script",
-    EditChat: "mdifier avec TubeChat",
+    EditChat: "Personnalisez dans votre style",
 
     Key_words: "Mots Clés",
     Description: "Description :",
@@ -79,12 +79,12 @@ const texts = {
     Title: "Titre",
     Video_paragraph: "Script :",
     Hooks: "Hooks : ",
-    Video_description: "Description vidéo : ",
-    Video_intro: "Intro vidéo",
-    mood: "Audience ciblé tranche d'age :",
+    Video_description: "Description : ",
+    Video_intro: "Introduction : ",
+    mood: "Audience ciblé :",
     french: "Français",
     english: "Anglais",
-    Target_audience_age_group: "Groupe d'âge du public cible",
+    Target_audience_age_group: "Audience ciblé",
     items: items_fr,
     length: "Longeur de la video",
     langs: langs_fr,
